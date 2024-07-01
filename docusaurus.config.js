04cf34860eea987136802781e1df0aebe066759d
QiaoJianFeng <845882161@qq.com>
2024-07-01 22:06:20 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 
 const config = {
-  title: 'FENG',
+  title: '蚂蚁号',
   tagline: 'Before you quit, Try',
   favicon: 'img/favicon.ico',
 
